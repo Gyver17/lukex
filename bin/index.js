5045d1373d20ceb63b338433a21cccf9fb89d481
Angel Cortez <acortez@cmlexports.com>
2023-05-02 01:50:59 -0400

    release: v1.0.3

--- bin/index.js
+++ bin/index.js
@@ -1,18 +1,21 @@
+import { existsSync } from 'fs';
 import path from "node:path";
 import minimist from "minimist";
 import { Plop, run } from "plop";
 
 const args = process.argv.slice(2);
 const argv = minimist(args);
+const localBinPathSegments = [process.cwd(), 'node_modules', 'lukex'];
 
-import { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
+const localBinExists = existsSync(path.join(...localBinPathSegments));
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const configPath = localBinExists ? path.join(...localBinPathSegments, 'src/plopfile.js') : path.join(process.cwd(), 'src/plopfile.js');
+
+console.log('configPath', configPath);
 
 Plop.prepare({
     cwd: argv.cwd,
-    configPath: path.join(__dirname, 'src/plopfile.js'),
+    configPath,
     preload: argv.preload || [],
     completion: argv.completion
 }, env => Plop.execute(env, run));
