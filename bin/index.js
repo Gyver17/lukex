32de16ae789d24d958f5ec22896780ebbb8e31ac
Angel Cortez <acortez@cmlexports.com>
2023-05-03 01:27:49 -0400

    bugfix: modify bin/index.js

--- bin/index.js
+++ bin/index.js
@@ -4,13 +4,22 @@ import path from "node:path";
 import minimist from "minimist";
 import { Plop, run } from "plop";
 
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
 const args = process.argv.slice(2);
 const argv = minimist(args);
 const localBinPathSegments = [process.cwd(), 'node_modules', 'lukex'];
 
 const localBinExists = existsSync(path.join(...localBinPathSegments));
 
-const configPath = localBinExists ? path.join(...localBinPathSegments, 'src/plopfile.js') : path.join(process.cwd(), 'src/plopfile.js');
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const localPath = localBinExists ? path.join(...localBinPathSegments, 'src/plopfile.js') : path.join(process.cwd(), 'src/plopfile.js');
+
+const localPathExists = existsSync(localPath);
+
+const configPath = localPathExists ? localPath : path.join(__dirname, '../src/plopfile.js');
 
 Plop.prepare({
     cwd: argv.cwd,
